0a9067303e9eb63b383dfe408d6448971cf33abb
mevshnu <vishnusivan1000@gmail.com>
2022-11-29 15:18:19 +0530

    pop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { HttpClientModule} from '@angular/common/http';
+
+
+const my:Routes=[
+  {
+    path:"",
+    component:ViewComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(my),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view/view.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css']
+})
+export class ViewComponent {
+
+  constructor(private api:ApiService)
+  {
+    api.Fetch().subscribe(
+      (response)=>
+      {
+        this.data=response;
+      }
+    )
+  }
+
+data:any=[]
+
+}
